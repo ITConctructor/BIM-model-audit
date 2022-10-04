9f7eb31bc83b6e1f2efcdd1d17b9b9f3d75f6812
Евгений Маслов <evgeniy.maslov@etalongroup.com>
2022-10-04 17:29:02 +0300

    Duplicate levels' checking is added (this is a test commit, because runtime generate a strange error)

--- Audit/Model/Checkings/IfLevelsAreNotDuplicatedChecking.cs
+++ Audit/Model/Checkings/IfLevelsAreNotDuplicatedChecking.cs
@@ -7,13 +7,13 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.Attributes;
 using System.ComponentModel;
+using Autodesk.Revit.ApplicationServices;
 
 namespace Audit.Model.Checkings
 {
     [Transaction(TransactionMode.Manual)]
     public class IfLevelsAreNotDuplicated : CheckingTemplate
     {
-        //pushing test after online merging
         public IfLevelsAreNotDuplicated()
         {
             Name = "ОБЩ_Уровни не дублируются";
@@ -21,13 +21,63 @@ namespace Audit.Model.Checkings
         }
         public override void Run(string filePath, BindingList<ElementCheckingResult> oldResults)
         {
-            ElementCheckingResult newResult = new ElementCheckingResult() { Name = "elementName", ID = "elementID", Time = System.DateTime.Now.ToString() };
-            ElementCheckingResult newResult2 = new ElementCheckingResult() { Name = "elementName2", ID = "elementID2", Time = System.DateTime.Now.ToString() };
-            ApplicationViewModel.AddElementCheckingResult(newResult2, oldResults);
-            ApplicationViewModel.AddElementCheckingResult(newResult, oldResults);
-            TaskDialog dialog = new TaskDialog("Test");
-            dialog.MainContent = Name;
-            dialog.Show();
+            //Открытие документа с отсоединением и закрытием всех рабочих наборов
+            UIApplication uiapp = CommandLauncher.uiapp;
+            ModelPath path = ModelPathUtils.ConvertUserVisiblePathToModelPath(filePath);
+            OpenOptions openOptions = new OpenOptions();
+            openOptions.DetachFromCentralOption = DetachFromCentralOption.DetachAndPreserveWorksets;
+            WorksetConfiguration worksets = new WorksetConfiguration(WorksetConfigurationOption.CloseAllWorksets);
+            openOptions.SetOpenWorksetsConfiguration(worksets);
+            Application app = CommandLauncher.app;
+            Document doc = app.OpenDocumentFile(path, openOptions);
+
+            //Получаем уровни
+            IList<Element> levels = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Levels).WhereElementIsNotElementType().ToElements();
+            IList<Element> results = new List<Element>();
+
+            //Получаем отметки уровней
+            List<double> marks = new List<double>();
+            foreach (Element level in levels)
+            {
+                marks.Add(Math.Round(level.LookupParameter("Фасад").AsDouble()*304.8, 0));
+            }
+
+            //Проверяем наличие повторяющихся отметок
+            for (int i = 0; i < marks.Count; i++)
+            {
+                if (marks.Count(x => x.Equals(marks[i])) > 1)
+                {
+                    results.Add(levels[i]);
+                }
+            }
+
+            //Из списка элементов заполняем отчет
+            foreach (Element element in results)
+            {
+                ElementCheckingResult result = new ElementCheckingResult() { Name = element.Name, ID = element.Id.ToString(), Time = System.DateTime.Now.ToString() };
+                ApplicationViewModel.AddElementCheckingResult(result, oldResults);
+            }
+
+            //Проверяем, есть ли среди прошлого результата проверок какой-либо результат из новой. Если нет, то ставим этому результату статус "Исправленная"
+            foreach (ElementCheckingResult item in oldResults)
+            {
+                int flag = 0;
+                foreach (Element level in results)
+                {
+                    if (item.Name == level.Name)
+                    {
+                        flag = 1;
+                    }
+                    if (flag == 1)
+                    {
+                        break;
+                    }
+                }
+                if (flag == 0)
+                {
+                    item.Status = "Исправленная";
+                }
+            }
         }
     }
 }
